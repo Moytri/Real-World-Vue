9452320579432d1ce5ea105930ded4e7ac2836f8
moytri <moytritopa@gmail.com>
2020-04-26 13:10:31 -0700

    rename the router.js and add router

---
+++ real-world-vue/src/router/router.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+import EventCreate from "../views/EventCreate.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "event-list",
+    component: EventList
+  },
+  {
+    path: "/event",
+    name: "event-show",
+    component: EventShow
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/create",
+    name: "event-create",
+    component: EventCreate 
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
