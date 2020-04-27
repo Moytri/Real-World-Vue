daa45e912639b547fdeb3bfc36f08e3acc4b6f35
moytri <moytritopa@gmail.com>
2020-04-26 19:31:16 -0700

    add dynamic route, use history mode

--- real-world-vue/src/router/router.js
+++ real-world-vue/src/router/router.js
@@ -13,14 +13,10 @@ const routes = [
     component: EventList
   },
   {
-    path: "/event",
-    name: "event-show",
-    component: EventShow
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () =>
-    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
   },
   {
     path: "/create",
@@ -30,6 +26,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 });
 
