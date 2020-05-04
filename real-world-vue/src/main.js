2a85b0a1f41aa5e762e31f10faefe31e3329b74d
moytri <moytritopa@gmail.com>
2020-05-03 22:08:44 -0700

    Register the global component

--- real-world-vue/src/main.js
+++ real-world-vue/src/main.js
@@ -2,7 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router.js";
 import store from "./store";
+import BaseIcon from '@/components/BaseIcon.vue';
 
+// globaly register the BaseIcon Component
+Vue.component('BaseIcon', BaseIcon);
 Vue.config.productionTip = false;
 
 new Vue({
